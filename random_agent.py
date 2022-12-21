f48969204461f61bc07ff7ee2f16cd59f7bbd1e5
DavidJong05 <36602654+DavidJong05@users.noreply.github.com>
2022-12-21 12:09:52 +0100

    Update random_agent.py

--- random_agent.py
+++ random_agent.py
@@ -2,14 +2,16 @@ import random
 import gomoku
 import copy
 import math
+import time
 from gomoku import Board, Move, GameState, valid_moves, check_win
 
 class tree_node():
-    def __init__(self, state, parentNode = None, last_move = None):
+    def __init__(self, state, parentNode = None, last_move = None, v_moves = None):
         self.state = state
         self.parent=parentNode
         self.children=[]
         self.last_move = last_move
+        self.v_moves = v_moves
         self.Q = 0
         self.N = 0
 
@@ -57,54 +59,55 @@ class random_dummy_player:
         """
         self.black = black_
 
-    def find_unexplored_move(self, moves: list, children: list):
-        if moves == []:
-            return None
-        for i in moves:
-            if i in children:
-                moves.remove(i)
+    def result_of_game(self, ply, player):
+        if player: # black = 1 = true
+            if ply % 2 == 0:
+                return 1
+            elif ply % 2 == 1:
+                return -1
             else:
-                return i
+                return 0
+        if player == False: # white = 2 = false
+            if ply % 2 == 1:
+                return 1
+            elif ply % 2 == 0:
+                return -1
+            else:
+                return 0
 
     def expand(self, node: tree_node) -> tree_node:
-        if check_win(node.state[0], node.last_move) or len(valid_moves(node.state)) == 0: # if n is terminal (game finished)
+        if check_win(node.state[0], node.last_move) or len(node.v_moves) == 0:
             return node
 
-        v_moves = copy.deepcopy(valid_moves(node.state))
-        random.shuffle(v_moves)
-        if len(node.children) != len(v_moves): # node is fully expanded if its number of children is equal to the number of valid moves in that game state - ALDS Reader
-            copy_state = copy.deepcopy(node.state)
-            new_move = self.find_unexplored_move(v_moves, node.children)
+        valid_move_list = copy.deepcopy(node.v_moves)
+        copy_state = copy.deepcopy(node.state)
+
+        if len(node.children) != len(valid_move_list):
+            new_move = random.choice(valid_move_list)
             is_valid, is_new, new_state = gomoku.move(copy_state, new_move)
-            new_child_node = tree_node(new_state, node, new_move)
-            node.children.append(new_child_node) # a random not-yet-explored move is selected to add as a child node
+            valid_move_list.remove(new_move)
+            new_child_node = tree_node(new_state, node, new_move, valid_move_list)
+            node.children.append(new_child_node)
             return new_child_node
 
         return self.expand(node.highest_uct())
 
-    def rollout(self, node: tree_node, g_state: GameState) -> float:
-        if gomoku.check_win(g_state[0], node.last_move):
-            if node.state[1] % 2 != self.black:
-                return 1
-            if node.state[1] % 2 == self.black:
-                return -1
-            else:
-                return 0
-        moves = copy.deepcopy(valid_moves(g_state))
+    def rollout(self, node) -> float:
+        if check_win(node.state[0], node.last_move) or len(node.v_moves) == 0:
+            return self.result_of_game(node.state[1], self.black)
+        moves = copy.deepcopy(node.v_moves)
         random.shuffle(moves)
-        while len(moves) > 0: # cant pop() when len(moves) = 0
+        new_state = copy.deepcopy(node.state)
+        while len(moves) > 0:  # cant pop() when len(moves) = 0
             random_move = moves.pop()
-            is_valid, is_win, g_state = gomoku.move(g_state, random_move) # random move in state S
+            is_valid, is_win, new_state = gomoku.move(new_state, random_move)  # random move in state S
 
-        if gomoku.check_win(g_state[0], node.last_move):
-            if node.state[1] % 2 != self.black:
-                return 1
-            if node.state[1] % 2 == self.black:
-                return -1
-        else: #draw
+        if check_win(new_state[0], node.last_move):
+            return self.result_of_game(new_state[1], self.black)
+        elif len(moves) == 0:
             return 0
 
-    def backup(self, value: float, node: tree_node) -> None:
+    def backup(self, value: float, node) -> None:
         while node is not None:
             node.N += 1
             node.Q += value
@@ -117,15 +120,17 @@ class random_dummy_player:
         3) the available moves you can play (this is a special service we provide ;-) )
         4) the maximum time until the agent is required to make a move in milliseconds [diverging from this will lead to disqualification].
         """
-
-        root = tree_node(copy.deepcopy(state), None, last_move)
-        while max_time_to_move > 0:
-            leaf = self.expand(root)
+        startTime = time.time_ns()
+        v_moves = copy.deepcopy(valid_moves(state))
+        root = tree_node(copy.deepcopy(state), None, last_move, v_moves)
+        counter = 0
+        while (((time.time_ns() - startTime) / 1000000) < max_time_to_move):
+            counter+=1
+            child = self.expand(root)
             for i in range(8):
-                val = self.rollout(leaf, copy.deepcopy(state))
-                self.backup(val, leaf)
-            max_time_to_move -=1
-
+                val = self.rollout(child)
+                self.backup(val, child)
+        print(f"{str(counter)} amount of loops")
         return root.bestest_move()
 
     def id(self) -> str:
